3db21332738a2e903db1aa8cc9ed6db9751ca73a
Nhan <tntn@3141@gmail.com>
2023-05-17 17:00:03 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import Multer from "multer";
 
