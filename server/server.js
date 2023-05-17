e604af9974f6df5d6e78753deeaeaead52149268
Nhan <tntn@3141@gmail.com>
2023-05-17 20:10:00 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(
   })
 );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://little-bookstore.netlify.app/");
+  res.setHeader("Access-Control-Allow-Origin", "https://little-bookstore.netlify.app");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
