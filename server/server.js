d7419fb3b5ff5d5fd9d607e70f71f6b3eb065572
Nhan <tntn@3141@gmail.com>
2023-05-17 11:24:51 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ const multer = Multer({
 app.use(
   cors({
     credentials: true,
-    origin: "http://127.0.0.1:5173",
+    origin: "https://little-bookstore.netlify.app/",
   })
 );
 app.use(express.json());
