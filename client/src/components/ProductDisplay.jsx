e11654080e62f23871d2c2cddf7d6afa6f5db286
Nhan <tntn@3141@gmail.com>
2023-05-20 10:40:44 +0700

    fixed axios url

--- client/src/components/ProductDisplay.jsx
+++ client/src/components/ProductDisplay.jsx
@@ -12,8 +12,7 @@ export default function ProductDisplay() {
   const [limit, setLimit] = useState(5);
   const isFirstRender = useFirstRender();
 
-  const url = "https://little-bookstore-api.fly.dev/api/books";
-  const { dataMore, loadingMore, errorMore, refetchMore } = useFetchMore(url, {
+  const { dataMore, loadingMore, errorMore, refetchMore } = useFetchMore("/api/books", {
     params: { normal: true, _skip: skip, _limit: limit },
   });
 
