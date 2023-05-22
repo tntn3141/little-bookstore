f4a675900e99f09ed9652ef2476fd700d0399356
Nhan <tntn@3141@gmail.com>
2023-05-22 18:50:18 +0700

    commit

--- client/src/components/LoadingIcon.jsx
+++ client/src/components/LoadingIcon.jsx
@@ -2,7 +2,7 @@ import loadingdot from "../assets/images/loadingdot.gif";
 
 function LoadingIcon() {
   return (
-    <div className="flex w-full h-full items-center justify-center m-24">
+    <div>
       <img src={loadingdot} alt="Loading..." className="mt-[152px]" />
     </div>
   );
