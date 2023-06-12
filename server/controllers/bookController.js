a7e1e28dec7b2c3dc70861e35c7172551342fbc8
Nhan <tntn@3141@gmail.com>
2023-06-12 11:17:36 +0700

    fixed latest books

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -69,7 +69,7 @@ export const getBooks = async (req, res, next) => {
   // NEW BOOKS (NEWEST -> OLDEST)
   if (latest) {
     try {
-      const latestBooks = await BookModel.find().limit(limit).skip(skip);
+      const latestBooks = await BookModel.find().sort({$natural: -1}).limit(limit).skip(skip);
       return res.status(200).json(latestBooks);
     } catch (error) {
       next(error);
