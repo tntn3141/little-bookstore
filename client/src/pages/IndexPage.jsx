6a4d8d0550f66563158e92cc0b51cd5a00cf4976
Nhan <tntn@3141@gmail.com>
2023-06-13 11:35:19 +0700

    commit

--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -1,5 +1,6 @@
 import ProductDisplay from "../components/ProductDisplay";
 import Hero from "../components/Hero";
+import SearchSidebar from "../components/SearchSidebar";
 
 export default function IndexPage() {
   return (
