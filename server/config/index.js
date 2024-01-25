d77e34ce927954b98e5e44a6224c3f5b49ce0034
Nhan <tntn@3141@gmail.com>
2024-01-25 11:12:08 +0700

    commit

--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const serviceKey = path.join(__dirname, process.env.GC_JSON);
+const serviceKey = path.join(__dirname, "./gc.json");
 
 const { Storage } = Cloud;
 const storage = new Storage({
