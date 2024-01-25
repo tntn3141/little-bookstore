ae66cdbb4246473260fa1689c6ed411f8892a3e7
Nhan <tntn@3141@gmail.com>
2024-01-25 11:02:48 +0700

    commit

--- server/config/index.js
+++ server/config/index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const serviceKey = path.join(__dirname, "./notional-cab-384210-146e9e056a32.json");
+const serviceKey = path.join(__dirname, process.env.GC_JSON);
 
 const { Storage } = Cloud;
 const storage = new Storage({
